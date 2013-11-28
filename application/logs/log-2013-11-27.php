1d3b4bc93d424c9a262da30c5c145e3048fd1da8
Henry Boldizsar <boldizsarhenry@gmail.com>
2013-11-27 18:41:21 -0600

    Removed br from search result

--- application/logs/log-2013-11-27.php
+++ application/logs/log-2013-11-27.php
@@ -25,3 +25,6 @@ ERROR - 2013-11-27 16:07:36 --> Severity: Notice  --> Undefined variable: search
 ERROR - 2013-11-27 16:11:18 --> Severity: Notice  --> Undefined variable: search_query C:\BitNami\wampstack-5.4.22-0\apache2\htdocs\application\modules\pages\views\search_field.php 6
 ERROR - 2013-11-27 16:30:10 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
  C:\BitNami\wampstack-5.4.22-0\apache2\htdocs\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-11-27 16:33:45 --> Severity: Warning  --> mysql_pconnect(): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\BitNami\wampstack-5.4.22-0\apache2\htdocs\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2013-11-27 16:33:45 --> Unable to connect to the database
--- application/modules/pages/views/search_results.php
+++ application/modules/pages/views/search_results.php
@@ -3,7 +3,7 @@
         foreach($query->result() as $row) {
             $name = $row->name;
             $link = '/confessions/view/'.$row->id;
-            echo "<div class='post'><h2><a class='red' href='".$link."'>".$name.'</a></h2></div><br>';
+            echo "<div class='post'><h2><a class='red' href='".$link."'>".$name.'</a></h2></div>';
         }
 
 ?>
