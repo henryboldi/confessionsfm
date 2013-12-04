64dbd564ba82c568db6c9f54f5f212295e71fe3f
Henry Boldizsar <boldizsarhenry@gmail.com>
2013-12-03 18:54:40 -0600

    removed confess link on latest

--- application/modules/confessions/views/latest.php
+++ application/modules/confessions/views/latest.php
@@ -2,9 +2,6 @@
 
 $this->load->module('pages');
 //echo '<h1>'.$this->pages->page_name($this->uri->segment(3)).'</h1>';
-echo "<div class='post' id='first'>";
-echo anchor('confessions/create/'.$this->uri->segment(3), 'Confess');
-echo '</div>';
 foreach ($query->result() as $row) {
     $edit_url = base_url().'confessions/'.$row->id;
     //echo "<div id='".$row->id."'class='post'><a class='red' href='/confessions/view/".$row->page_id."'>".$this->pages->page_name($this->uri->segment(3))."</a><p>".$row->confession.'</p> at '.$row->confession_date_time;
