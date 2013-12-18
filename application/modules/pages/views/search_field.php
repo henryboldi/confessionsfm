9f51a57c8150860534cdd3aeeab140dacab98003
Henry Boldizsar <henry@wolverine.(none)>
2013-12-17 19:40:10 -0600

    added placeholder to search

--- application/modules/pages/views/search_field.php
+++ application/modules/pages/views/search_field.php
@@ -3,8 +3,8 @@ $form_attr = 'class="searchbox"';
 echo form_open('pages/search', $form_attr);
 
 //echo "<div class='post'>Search:";
-$field_attr = 'id="search"';
-echo form_input('search_query', '', $field_attr);
+$field_attr = 'id="search" placeholder="Search for a confessions group"';
+echo form_input('search_query', NULL, $field_attr);
 
 
 echo form_submit('submit', 'Search');
